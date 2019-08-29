0bdf5490a49a31ab31da9e673b481b84f6de4093
Samridh <samridhharsh@gmail.com>
2019-08-29 10:07:51 +0530

    final commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
-    
-  var cartCount = 0;
-  
+  var Count = 0;
+  var cartCount = "courses in cart count: ";
+
   $("a.direct").click(function() {
-     if( $(this).hasClass('selected-cart') ) {
-         // ignore
-     } else {
-         $(this).addClass('selected-cart');
-         cartCount++;
-     }
+    if ($(this).hasClass("selected-cart")) {
+      alert('item already in cart');
+    } else {
+      $(this).addClass("selected-cart");
+      Count++;
 
-     console.log(cartCount)
-  })
+    }
+    SetCartOnview(cartCount + Count);
+    
+  });
 
-})
+  $("a.remove").click(function() {
+    if ($(this).hasClass("selected-cart")) {
+        alert('item not present in cart');
+    } else {
+        $(this).addClass("selected-cart");
+        Count--;
+
+    }
+    SetCartOnview(cartCount + Count);
+    
+  });
+
+  function SetCartOnview(c) {
+    $("#cartCount").html(c);
+  }
+});
